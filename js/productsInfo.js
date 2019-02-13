6bb133d29054b3bf4c4aee74be00d531316044e8
Daniele Fusari <danieledoughty@Danieles-MacBook-Pro.local>
2019-02-13 10:04:02 +0000

    added all the products to the info page and fixed layout in T&C and About pages

--- js/productsInfo.js
+++ js/productsInfo.js
@@ -1,14 +1,65 @@
+// ************ Hair **************
+
+// Shampoo
 const claynopoo = {
   name: 'Clay Nopoo',
-  img: 'img/products/IMG_8114.jpg',
-  info: "orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-  ingreadence: "the printing, and typesetting, industry, Lorem Ipsum, has been the industry's, standard dummy, text ever since the 1500s"
+  img: 'img/products/Nopoo.jpg',
+  info: "TBA",
+  ingreadence: "TBA"
 }
 const prewashoilconditioner = {
   name: 'Pre Wash Oil Conditioner',
-  img: 'img/products/IMG_8120.jpg',
-  info: "orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-  ingreadence: "the printing, and typesetting, industry, Lorem Ipsum, has been the industry's, standard dummy, text ever since the 1500s"
+  img: 'img/products/conditioner.jpg',
+  info: "TBA",
+  ingreadence: "TBA"
+}
+// ************ END Hair **************
+
+
+// ************ Face **************
+
+// Oil Washes
+
+const lavenderfaceoilwash = {
+  name: 'Lavender face Oil Wash',
+  img: 'img/products/oilwash.jpg',
+  info: "TBA",
+  ingreadence: "TBA"
+}
+
+const acneoilwash = {
+  name: 'Acne Oil Wash',
+  img: 'img/products/acneoil.jpg',
+  info: "TBA",
+  ingreadence: "TBA"
+}
+
+const ealmingoilwash = {
+  name: 'Ealming oil Wash',
+  img: 'img/products/ealmingoil.jpg',
+  info: "TBA",
+  ingreadence: "TBA"
+}
+
+const facecreaminthesun = {
+  name: 'Face Cream in the sun',
+  img: 'img/products/facecreamsun.jpg',
+  info: "TBA",
+  ingreadence: "TBA"
+}
+
+// ************ END Face **************
+
+
+// ************ Body **************
+
+// Creams
+
+const creaminthesun = {
+  name: 'Cream in the Sun',
+  img: 'img/products/creaminthesun.jpg',
+  info: "TBA",
+  ingreadence: "TBA"
 }
 
 const crèmedelamagie = {
@@ -18,16 +69,50 @@ const crèmedelamagie = {
   ingreadence: 'Ingredients: Cocoa butter, shea butter, coconut oil, carrot seed oil and optional essential oils (individual requests made to order)'
 }
 
-const smudgedeodorant = {
-  name: 'Smudge Deodorant',
+const bumpbalm = {
+  name: 'Bump Balm',
   img: 'img/products/bumpbalm.jpg',
-  info: 'A natural deodorant with antibacterial properties ',
-  ingreadence: 'coconut oil, shea butter, arrowroot powder, corn starch, baking soda, magnesium oil, essential oils '
+  info: 'TBA',
+  ingreadence: 'TBA'
+}
+
+// Deoderant
+
+const peppemintdeo = {
+  name: 'Peppemint Deodorant',
+  img: 'img/products/peppemintdeo.jpg',
+  info: 'TBA',
+  ingreadence: 'TBA'
+}
+
+const lavinderdeo = {
+  name: 'Lavinder Deodorant',
+  img: 'img/products/lavinderdeo.jpg',
+  info: 'TBA',
+  ingreadence: 'TBA'
 }
 
 const smudgesensitivedeodorant = {
-  name: 'Smudge Sensitive Deodorant ',
-  img: 'img/products/bumpbalm.jpg',
-  info: ' TBC',
+  name: 'Smudge Sensitive Deodorant',
+  img: 'img/products/sensitivedeodorant.jpg',
+  info: 'TBA',
   ingreadence: 'coconut oil, shea butter, arrowroot powder, bentonite clay, essential oils '
 }
+
+const smudgeseodorant = {
+  name: 'Smudge Deodorant',
+  img: 'img/products/smudgeseodorant.jpg',
+  info: 'A natural deodorant with antibacterial properties',
+  ingreadence: 'coconut oil, shea butter, arrowroot powder, corn starch, baking soda, magnesium oil, essential oils '
+}
+
+// Body Oil
+
+const antiinflammationoil = {
+  name: 'Anti-Inflammation Oil',
+  img: 'img/products/inflammationoil.jpg',
+  info: 'TBA',
+  ingreadence: 'TBA '
+}
+
+// ************ END Body **************
