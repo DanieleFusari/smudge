20cc3e990f84747b2f3bdf05d9c86de6f7cc5fc2
Daniele Fusari <danieledoughty@Danieles-MacBook-Pro.local>
2019-02-20 11:50:26 +0000

    added obeject through using a class

--- js/productsInfo.js
+++ js/productsInfo.js
@@ -1,146 +1,44 @@
-// ************ Hair **************
+class Products {
+  constructor(name, img, info, ingreadence) {
+    this.name = name,
+    this.img = img,
+    this.info = info,
+    this.ingreadence = ingreadence
+  }
+}
 
+// ************ Hair **************
 // Shampoo
-const claynopoo = {
-  name: 'Clay Nopoo',
-  img: 'img/products/Nopoo.jpg',
-  info: "TBA",
-  ingreadence: "TBA"
-}
-const prewashoilconditioner = {
-  name: 'Pre Wash Oil Conditioner',
-  img: 'img/products/conditioner.jpg',
-  info: "TBA",
-  ingreadence: "TBA"
-}
+const claynopoo = new Products('Clay Nopoo', 'img/products/Nopoo.jpg', "TBA", "TBA")
+// Conditioner
+const prewashoilconditioner = new Products('Pre Wash Oil Conditioner', 'img/products/conditioner.jpg', "TBA", "TBA")
 // ************ END Hair **************
 
 
 // ************ Face **************
 
 // Oil Washes
-
-const lavenderfaceoilwash = {
-  name: 'Smudge Oil Wash Lavender and Rosemary Face Facial Oil Wash (acne prone skin)',
-  img: 'img/products/acneoil.jpg',
-  info: "This lavender and rosemary oil wash is designed for skin with acne including oily skin. Lavender is healing even as rosemary is antiseptic, antibacterial and helps balance oily skin. The oil wash is blended with borage oil which is high in oleic acids; acids which deeply penetrate the skins surface therefore replenishing and maintaining moisture without clogging pores. ",
-  ingreadence: "Grape seed oil, borage oil, hazelnut oil (castor oil as an alternative for those with nut allergies), lavender and rosemary essential oil"
-}
-
-const acneoilwash = {
-  name: 'Smudge Oil Wash Lavender and Rosemary Face Facial Oil Wash (acne prone skin)',
-  img: 'img/products/acneoil.jpg',
-  info: "This lavender and rosemary oil wash is designed for skin with acne including oily skin. Lavender is healing even as rosemary is antiseptic, antibacterial and helps balance oily skin. The oil wash is blended with borage oil which is high in oleic acids; acids which deeply penetrate the skins surface therefore replenishing and maintaining moisture without clogging pores. ",
-  ingreadence: "Grape seed oil, borage oil, hazelnut oil (castor oil as an alternative for those with nut allergies), lavender and rosemary essential oil"
-}
-
-const smudgebrighteningandhealingoilwash  = {
-  name: 'Smudge Brightening and Healing Oil Wash',
-  img: 'img/products/ealmingoil.jpg',
-  info: "Smudge's brightening oil wash contains lemongrass essential oil; an essential oil which can brighten and tone the skin. It also contains chamomile essential oil which is healing consequently good for soothing tired and inflamed skin. The base of sea buckthorn and rosehip oil makes the wash nourishing and regenerating.",
-  ingreadence: "Grape seed oil, borage oil, hazelnut oil (castor oil as an alternative for those with nut allergies), lemongrass and chamomile essential oil"
-}
-
-const antiagingfacialoilwash = {
-  name: 'Smudge Rose Geranium Anti Aging Facial Oil Wash ',
-  img: 'img/products/ealmingoil.jpg',
-  info: "Made with Rose Geranium essential oil, jojoba and rosehip oil meaning this oil wash is both healing, nourishing and anti aging. Geranium rose has anti-inflammatory and anti aging properties whilst rosehip oil is regenerating and anti-aging.",
-  ingreadence: "Apricot seed oil, jojoba oil, rosehip oil, hazelnut oil (castor oil as an alternative for those with nut allergies), and rose geranium essential oil"
-}
-
-const smudgefacialoilwashes = {
-  name: 'Smudge Facial Oil Washes',
-  img: 'img/products/ealmingoil.jpg',
-  info: "Oil wash for the face cleanses the skin without stripping out natural oils. Natural oils help lubricate, heal, protect and moisturise facial skin. Skin that is stripped of these naturally produced oils attempts to repair by quickly producing more oil perpetuating the washing cycle. This can lead to dry and/or oily skin. Smudge Oil Washes operate through oil dissolving oil allowing the skin to function properly.",
-  ingreadence: "How to use: Wet the skin with warm water then massage in the oil blend. Leave for several minutes (this could be done whilst brushing teeth or when taking a hot bath/ shower. Heat opens the pores allowing the oil wash to sink deeper into the skin). Wipe off the facial oil wash with a wet hot cloth. "
-}
-
-const facecreaminthesun = {
-  name: 'Smudge in the Sun\' face cream',
-  img: 'img/products/facecreamsun.jpg',
-  info: "'Smudge in the Sun' is a mineral non-comedogenic cream designed to be applied on sunny occasions. Being rich in shea butter, it is a good source of vitamins for skin. Similar to 'Smudge in the Sun' body cream, Smudge's face version contains zinc oxide known to have sun protective properties and does not contain toxic chemicals. Jojoba oil, vitamin e and carrot seed oil have their own natural sun protection whilst also being anti-inflammatory. (Disclaimer: Smudge does not claim this cream has a SPF and has not been tested for accurate SPF rating)",
-  ingreadence: "Shea butter, non nano zinc oxide, vitamin e oil, jojoba oil, essential oils (carrot seed and red raspberry seed oil)"
-}
-
+const lavenderfaceoilwash = new Products('Smudge Oil Wash Lavender and Rosemary Face Facial Oil Wash (acne prone skin)', 'img/products/acneoil.jpg', "This lavender and rosemary oil wash is designed for skin with acne including oily skin. Lavender is healing even as rosemary is antiseptic, antibacterial and helps balance oily skin. The oil wash is blended with borage oil which is high in oleic acids; acids which deeply penetrate the skins surface therefore replenishing and maintaining moisture without clogging pores. ", "Grape seed oil, borage oil, hazelnut oil (castor oil as an alternative for those with nut allergies), lavender and rosemary essential oil")
+const acneoilwash = new Products('Smudge Oil Wash Lavender and Rosemary Face Facial Oil Wash (acne prone skin)', 'img/products/acneoil.jpg', "This lavender and rosemary oil wash is designed for skin with acne including oily skin. Lavender is healing even as rosemary is antiseptic, antibacterial and helps balance oily skin. The oil wash is blended with borage oil which is high in oleic acids; acids which deeply penetrate the skins surface therefore replenishing and maintaining moisture without clogging pores. ", "Grape seed oil, borage oil, hazelnut oil (castor oil as an alternative for those with nut allergies), lavender and rosemary essential oil")
+const smudgebrighteningandhealingoilwash = new Products('Smudge Brightening and Healing Oil Wash', 'img/products/ealmingoil.jpg', "Smudge's brightening oil wash contains lemongrass essential oil; an essential oil which can brighten and tone the skin. It also contains chamomile essential oil which is healing consequently good for soothing tired and inflamed skin. The base of sea buckthorn and rosehip oil makes the wash nourishing and regenerating.", "Grape seed oil, borage oil, hazelnut oil (castor oil as an alternative for those with nut allergies), lemongrass and chamomile essential oil")
+const antiagingfacialoilwash = new Products('Smudge Rose Geranium Anti Aging Facial Oil Wash ', 'img/products/ealmingoil.jpg', "Made with Rose Geranium essential oil, jojoba and rosehip oil meaning this oil wash is both healing, nourishing and anti aging. Geranium rose has anti-inflammatory and anti aging properties whilst rosehip oil is regenerating and anti-aging.", "Apricot seed oil, jojoba oil, rosehip oil, hazelnut oil (castor oil as an alternative for those with nut allergies), and rose geranium essential oil")
+const smudgefacialoilwashes = new Products('Smudge Facial Oil Washes', 'img/products/ealmingoil.jpg', "Oil wash for the face cleanses the skin without stripping out natural oils. Natural oils help lubricate, heal, protect and moisturise facial skin. Skin that is stripped of these naturally produced oils attempts to repair by quickly producing more oil perpetuating the washing cycle. This can lead to dry and/or oily skin. Smudge Oil Washes operate through oil dissolving oil allowing the skin to function properly.", "How to use: Wet the skin with warm water then massage in the oil blend. Leave for several minutes (this could be done whilst brushing teeth or when taking a hot bath/ shower. Heat opens the pores allowing the oil wash to sink deeper into the skin). Wipe off the facial oil wash with a wet hot cloth. ")
+const facecreaminthesun = new Products('Smudge in the Sun\' face cream', 'img/products/facecreamsun.jpg', "'Smudge in the Sun' is a mineral non-comedogenic cream designed to be applied on sunny occasions. Being rich in shea butter, it is a good source of vitamins for skin. Similar to 'Smudge in the Sun' body cream, Smudge's face version contains zinc oxide known to have sun protective properties and does not contain toxic chemicals. Jojoba oil, vitamin e and carrot seed oil have their own natural sun protection whilst also being anti-inflammatory. (Disclaimer: Smudge does not claim this cream has a SPF and has not been tested for accurate SPF rating)", "Shea butter, non nano zinc oxide, vitamin e oil, jojoba oil, essential oils (carrot seed and red raspberry seed oil)")
 // ************ END Face **************
 
 
 // ************ Body **************
-
 // Creams
-
-const creaminthesun = {
-  name: 'Cream in the Sun',
-  img: 'img/products/creaminthesun.jpg',
-  info: "A mineral cream designed to be applied on sunny occasions. 'Smudge in the Sun' is made with minerals and oils which naturally have defensive and moisturising qualities; It contains zinc oxide known to have sun protective properties whilst not being absorbed into the skin. Jojoba oil, vitamin e and carrot seed oil have their own natural sun protection whilst also being anti-inflammatory. This cream does not contain toxic chemicals that are absorbed into the skin. (Disclaimer: Smudge does not claim this cream has a SPF and has not been tested for accurate SPF rating)",
-  ingreadence: "Shea butter, cocoa butter, coconut oil, non nano zinc oxide, vitamin e oil, jojoba oil and carrot seed oil"
-}
-
-const creaminthesunwithinsectrepellent = {
-  name: 'Cream in the Sun with insect repellent',
-  img: 'img/products/creaminthesun.jpg',
-  info: "Smudge in the Sun with insect repellent is a variant of Smudge in the Sun. Nevertheless, it has added essential oils which help repel insects and soothe inflamed skin. (Disclaimer: This cream is not claiming to have a SPF and has not been tested in the laboratory)",
-  ingreadence: "Shea butter, cocoa butter, coconut oil, non nano zinc oxide, vitamin e oil, jojoba oil and essential oils (lavender, cedar wood and peppermint) "
-}
-
-const crèmedelamagie = {
-  name: 'Crème de la magie',
-  img: 'img/products/cremedelamagie.jpg',
-  info: 'A natural and rich body cream designed to be moisturising and healing. Cocoa butter is high in fatty acids making it healing and nourishing.',
-  ingreadence: 'cocoa butter, shea butter, coconut oil, carrot seed oil and optional essential oils (individual requests made to order)'
-}
-
-const bumpbalm = {
-  name: 'Bump Balm',
-  img: 'img/products/bumpbalm.jpg',
-  info: 'A cream created with calendula oil known for its healing properties. The shea butter ingredient means it is rich it vitamins for skin. Use on bumps, inflamed skin, bruises and scars. Suitable for face and body due to non-comedogenic (non pore clogging) properties. ',
-  ingreadence: 'Calendula oil, shea butter, hemp seed oil, carrot seed oil and chamomile oil. '
-}
-
+const creaminthesun = new Products('Cream in the Sun', 'img/products/creaminthesun.jpg', "A mineral cream designed to be applied on sunny occasions. 'Smudge in the Sun' is made with minerals and oils which naturally have defensive and moisturising qualities; It contains zinc oxide known to have sun protective properties whilst not being absorbed into the skin. Jojoba oil, vitamin e and carrot seed oil have their own natural sun protection whilst also being anti-inflammatory. This cream does not contain toxic chemicals that are absorbed into the skin. (Disclaimer: Smudge does not claim this cream has a SPF and has not been tested for accurate SPF rating)", "Shea butter, cocoa butter, coconut oil, non nano zinc oxide, vitamin e oil, jojoba oil and carrot seed oil")
+const creaminthesunwithinsectrepellent = new Products('Cream in the Sun with insect repellent', 'img/products/creaminthesun.jpg', "Smudge in the Sun with insect repellent is a variant of Smudge in the Sun. Nevertheless, it has added essential oils which help repel insects and soothe inflamed skin. (Disclaimer: This cream is not claiming to have a SPF and has not been tested in the laboratory)", "Shea butter, cocoa butter, coconut oil, non nano zinc oxide, vitamin e oil, jojoba oil and essential oils (lavender, cedar wood and peppermint)")
+const crèmedelamagie = new Products('Crème de la magie', 'img/products/cremedelamagie.jpg', 'A natural and rich body cream designed to be moisturising and healing. Cocoa butter is high in fatty acids making it healing and nourishing.', 'cocoa butter, shea butter, coconut oil, carrot seed oil and optional essential oils (individual requests made to order)')
+const bumpbalm = new Products('Bump Balm', 'img/products/bumpbalm.jpg', 'A cream created with calendula oil known for its healing properties. The shea butter ingredient means it is rich it vitamins for skin. Use on bumps, inflamed skin, bruises and scars. Suitable for face and body due to non-comedogenic (non pore clogging) properties.', 'Calendula oil, shea butter, hemp seed oil, carrot seed oil and chamomile oil.')
 // Deoderant
-
-const peppemintdeo = {
-  name: 'Tea Tree and Peppermint Deodorant ',
-  img: 'img/products/peppemintdeo.jpg',
-  info: 'Smudge Tea Tree and Peppermint Deodorant is the ultimate anti bacterial and anti fungal product in the range.  Tea tree oil is renowned for powerful purifying properties making it ideal for deodorant. Peppermint adds an invigorating and refreshing quality.',
-  ingreadence: 'Shea butter, bentonite clay, coconut oil, magnesium oil, tea tree and peppermint essential oil'
-}
-
-const lavinderdeo = {
-  name: 'Lavinder Deodorant',
-  img: 'img/products/lavinderdeo.jpg',
-  info: 'Smudge Gentle Deodorant is made with lavender essential oil. Lavender is one of the gentlest essential oils yet also has healing and antibacterial properties. ',
-  ingreadence: 'Shea butter, bentonite clay, coconut oil, magnesium oil, lavender essential oil'
-}
-
-const smudgecitrusdeodorant  = {
-  name: 'Smudge Citrus Deodorant ',
-  img: 'img/products/lavinderdeo.jpg',
-  info: 'Smudge Citrus Deodorant, whilst being antibacterial and antifungal, has an uplifting citrusy aroma.',
-  ingreadence: 'Shea butter, bentonite clay, coconut oil, magnesium, bergamot essential oil'
-}
-
-const smudgesensitivedeodorant = {
-  name: 'Smudge Sensitive Deodorant',
-  img: 'img/products/sensitivedeodorant.jpg',
-  info: 'TBA',
-  ingreadence: 'TBA'
-}
-
-const smudgedeodorant = {
-  name: 'Smudge Deodorant',
-  img: 'img/products/smudgeseodorant.jpg',
-  info: 'All of the Smudge natural deodorants are made with bentonite clay to be gentle, antibacterial and absorbent. Many deodorants on the market contain chemicals such as aluminium, parabens and steareths; Smudge deodorants are made with natural raw ingredients which do not clog pores or contain harmful chemicals.',
-  ingreadence: 'shea butter, bentonite clay, coconut oil, magnesium oil and essential oils'
-}
-
+const peppemintdeo = new Products('Tea Tree and Peppermint Deodorant ', 'img/products/peppemintdeo.jpg', 'Smudge Tea Tree and Peppermint Deodorant is the ultimate anti bacterial and anti fungal product in the range.  Tea tree oil is renowned for powerful purifying properties making it ideal for deodorant. Peppermint adds an invigorating and refreshing quality.', 'Shea butter, bentonite clay, coconut oil, magnesium oil, tea tree and peppermint essential oil')
+const lavinderdeo = new Products('Lavinder Deodorant', 'img/products/lavinderdeo.jpg', 'Smudge Gentle Deodorant is made with lavender essential oil. Lavender is one of the gentlest essential oils yet also has healing and antibacterial properties. ', 'Shea butter, bentonite clay, coconut oil, magnesium oil, lavender essential oil')
+const smudgecitrusdeodorant  = new Products('Smudge Citrus Deodorant ', 'img/products/lavinderdeo.jpg', 'Smudge Citrus Deodorant, whilst being antibacterial and antifungal, has an uplifting citrusy aroma.', 'Shea butter, bentonite clay, coconut oil, magnesium, bergamot essential oil')
+const smudgesensitivedeodorant = new Products('Smudge Sensitive Deodorant', 'img/products/sensitivedeodorant.jpg', 'TBA', 'TBA')
+const smudgedeodorant = new Products('Smudge Deodorant', 'img/products/smudgeseodorant.jpg', 'All of the Smudge natural deodorants are made with bentonite clay to be gentle, antibacterial and absorbent. Many deodorants on the market contain chemicals such as aluminium, parabens and steareths; Smudge deodorants are made with natural raw ingredients which do not clog pores or contain harmful chemicals.', 'shea butter, bentonite clay, coconut oil, magnesium oil and essential oils')
 // Body Oil
-
-const antiinflammationoil = {
-  name: 'Anti-Inflammation Oil',
-  img: 'img/products/inflammationoil.jpg',
-  info: 'An oil blend designed to massage into aching joints. Research has shown that this specific blend has been shown to decrease pain scores in patients with arthritis. Eucalyptus, lavender, marjoram, rosemary and peppermint essential oils blend together with carrier oils to help provide relief for painful areas. ',
-  ingreadence: ' Apricot kernal oil, sweet almond oil, jojoba oil and essential oils (eucalyptus, lavender, marjoram, rosemary and peppermint)'
-}
-
+const antiinflammationoil = new Products('Anti-Inflammation Oil', 'img/products/inflammationoil.jpg', 'An oil blend designed to massage into aching joints. Research has shown that this specific blend has been shown to decrease pain scores in patients with arthritis. Eucalyptus, lavender, marjoram, rosemary and peppermint essential oils blend together with carrier oils to help provide relief for painful areas. ',  ' Apricot kernal oil, sweet almond oil, jojoba oil and essential oils (eucalyptus, lavender, marjoram, rosemary and peppermint)')
 // ************ END Body **************
